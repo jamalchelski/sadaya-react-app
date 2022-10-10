465e0ad5b0178d8a90c98468b67ecd1d63fd8073
jamalChelski <ifista202522035@gmail.com>
2022-10-10 23:24:51 +0700

    site up

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-// import Main from "./pages/home/Main";
-import Maintenance from "./pages/Maintenance/Maintenance";
+import Main from "./pages/home/Main";
+// import Maintenance from "./pages/Maintenance/Maintenance";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    {/* <Main /> */}
-    <Maintenance />
+    <Main />
+    {/* <Maintenance /> */}
   </React.StrictMode>
 );
 
