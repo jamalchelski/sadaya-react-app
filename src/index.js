e9ac828f5de9cd821f572f2992fe29ab3fbfb652
jamalChelski <ifista202522035@gmail.com>
2022-10-10 22:59:02 +0700

    maintenance Page

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import Main from "./pages/home/Main";
+// import Main from "./pages/home/Main";
+import Maintenance from "./pages/Maintenance/Maintenance";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <Main />
+    {/* <Main /> */}
+    <Maintenance />
   </React.StrictMode>
 );
 
---
+++ src/pages/Maintenance/Maintenance.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Styles.css";
+
+function Maintenance() {
+  return (
+    <article>
+      <h1>We&rsquo;ll be back soon!</h1>
+      <div>
+        <p>
+          Sorry for the inconvenience. We&rsquo;re performing some maintenance
+          at the moment. If you need to you can always follow us on{" "}
+          <a href="https://github.com/jamalchelski">Github</a> for updates,
+          otherwise we&rsquo;ll be back up shortly!
+        </p>
+        <p>&mdash; The [MJ-PROD] Team</p>
+      </div>
+    </article>
+  );
+}
+
+export default Maintenance;
